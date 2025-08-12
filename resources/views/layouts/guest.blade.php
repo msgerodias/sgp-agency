d8e9e080a94775b841ccc5527a2820d17f0f37f9
ste <34229614+msgerodias@users.noreply.github.com>
2025-08-12 18:24:18 +0800

    up

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,16 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#111827]">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-[#1f2937] shadow-md overflow-hidden sm:rounded-lg border-4 border-[#FFD700]">
+                <a href="{{ url('/') }}" class="flex justify-center mb-4">
+                    <img src="{{ asset('images/sgplogo.png') }}" alt="SGP Agency" class="h-64">
                 </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
+
         </div>
     </body>
 </html>
